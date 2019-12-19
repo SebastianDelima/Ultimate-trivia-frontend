b78c33959227a5680577964dd4d19877e4cb48b6
Sebastian Delima <sebastiandelima@Cactus1.local>
2019-12-19 18:32:40 -0500

    first question laggy

--- html/starterCode.js
+++ html/starterCode.js
@@ -1,25 +1,88 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     console.log("you are here")
-    subjectsFetch()
-    questionsFetch()
-    gameSessions()
+    starter()
 })
 
 
-function subjectsFetch(){
-    fetch('http://localhost:3000/subjects')
-    .then(response => response.json())
-    .then(tests => {debugger})
+function starter(){
+    document.querySelector('#history-button').addEventListener('click', fetchHistory)
+
 }
 
-function questionsFetch(){
-    fetch('http://localhost:3000/questions')
+function fetchHistory(){
+   let counter = 1
+   let level = 0
+    fetch('http://localhost:3000/subjects/5')
     .then(response => response.json())
-    .then(questions => { })
+    .then(subject  => displayHistoryQuestions(subject, counter, level))
+    
 }
 
-function gameSessions(){
-    fetch('http://localhost:3000/game_sessions')
-    .then(response => response.json())
-    .then(games => {})
+
+function displayHistoryQuestions(subject, counter, level){
+
+    document.querySelector('#buttons').remove()
+
+    let sec = 10;
+    let body = document.querySelector('body')
+    let timerDisplay = document.createElement('p')
+    timerDisplay.setAttribute('id', 'timerDisplay')
+    body.append(timerDisplay)
+    
+    let timer = setInterval(function(){
+        document.getElementById('timerDisplay').innerText='00:'+sec;
+        sec--;
+        if (sec === 9){
+
+            let body = document.querySelector('body')
+        
+            let questionName     = subject.questions[level].name
+            let questionAns1     = subject.questions[level].answer1
+            let questionAns2     = subject.questions[level].answer2
+            let questionAns3     = subject.questions[level].answer3
+
+            let timerDiv         = document.createElement('div')
+            let levelCounter     = document.createElement('div')
+            let timerTitle       = document.createElement('h2')        
+            let selectForm       = document.createElement('select')
+            let questionDiv      = document.createElement('div')
+            let questionNameNode = document.createElement('h1')
+            let ans1Node         = document.createElement('option')
+            let ans2Node         = document.createElement('option')
+            let ans3Node         = document.createElement('option')
+            
+            timerDiv.setAttribute('id','timer')
+            questionDiv.setAttribute('id', 'question-div')
+            selectForm.setAttribute('id', 'choices')
+            selectForm.setAttribute('multiple', 'multiple')
+            questionNameNode.setAttribute('id', 'questionName')
+            ans1Node.setAttribute('id', 'ans1')
+            ans2Node.setAttribute('id', 'ans2')
+            ans3Node.setAttribute('id', 'ans3')
+        
+
+            levelCounter.innertext     = `Question: ${counter}`
+            timerTitle.innerText       = 'Timer'
+            questionNameNode.innerText = questionName
+            ans1Node.innerText         = questionAns1
+            ans2Node.innerText         = questionAns2
+            ans3Node.innerText         = questionAns3
+            
+            
+            timerDiv.append(levelCounter, timerTitle, timerDisplay)
+            body.appendChild(timerDiv)
+            body.appendChild(questionDiv)
+            questionDiv.append(questionNameNode, selectForm)
+            selectForm.append(ans1Node, ans2Node, ans3Node)
+        
+        }else if (sec < 0) {
+           lose()
+            clearInterval(timer);
+        }
+    }, 1000);
+    
+}
+
+function lose(){
+    console.log("you lose!")
 }
